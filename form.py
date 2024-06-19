67807f8598b47a41fd6ac11a56f3b5c155d51e99
KarnKalia <karnkalia@gmail.com>
2024-06-19 13:31:26 +1000

    Template Created

---
+++ form.py
@@ -0,0 +1,53 @@
+from tkinter import *
+import customtkinter as ctk
+
+
+root = ctk.CTk()
+root.geometry("500x500")
+root.title("Possession Form")
+
+logo = ctk.CTkLabel(root, text='LOGO')
+logo.place(x=20, y=10, relx=0.01, rely=0.01)
+
+
+# Widgets 
+
+# 0. ID (Title - Company_Project_Station_PossessionType_PossessionNo._Year)
+
+# 1. Drop down Menu - Select Company 
+
+# 2. Drop down Menu - Select Project
+
+# 3. Drop down Menu - Select Station
+
+# 4. Drop down Menu - Select Possession Type
+
+# 5. Drop down Menu - Select Possession No. 
+
+# 6. Select Start Date
+
+# 7. Finish Date
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
